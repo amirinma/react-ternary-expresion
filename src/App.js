a827d4b09063e45c6c9788c997bea5bd7c049b09
Mohammad <zia.ro@live.com>
2022-12-12 22:21:06 -0500

    First Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
+  const [display, setDisplay]=React.useState(["Zia", "Asna"])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+      display.length === 0 ? 
+      <h6>You are all caught up!</h6>:
+      <h6>You have {display.length} unread {display.length > 1 ? "messages." : "message."}</h6>
+     }
     </div>
   );
 }
